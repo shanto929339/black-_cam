9ca31d55636da1b669407b6d10c36d7ae729064b
shanto929339 <shantokhna929339@gmail.com>
2024-09-05 15:34:50 +0600

    vedio recording feature done

--- android/app/src/main/kotlin/com/example/black_camera/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/black_camera/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.example.black_camera
 
-import io.flutter.embedding.android.FlutterActivity
+//import io.flutter.embedding.android.FlutterActivity
+//
+//class MainActivity: FlutterActivity()
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterFragmentActivity() {}
